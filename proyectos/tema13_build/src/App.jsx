f6c80e86981415a2ba780024f7c3f02ef01ec4ab
Alejandro Lobo <alejandro.lobo@azetti.com>
2025-06-16 12:23:19 +0200

    Tema 13: build

---
+++ proyectos/tema13_build/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+import VariablesEntorno from "./components/VariablesEntorno";
+
+function App() {
+  return (
+    <div className="App">
+      <VariablesEntorno />
+    </div>
+  );
+}
+
+export default App;
---
+++ proyectos/tema13_build/src/components/VariablesEntorno.jsx
@@ -0,0 +1,5 @@
+const VariablesEntorno = () => {
+    return <div>Entorno: {import.meta.env.VITE_ENTORNO}</div>;
+};
+
+export default VariablesEntorno;
